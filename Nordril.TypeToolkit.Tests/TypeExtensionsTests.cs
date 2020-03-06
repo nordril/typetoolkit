9a51ab0d038d666477606f35e88d86eb68e32d9a
Janos Tapolczai <janos.tapolczai@nordril.com>
2020-03-06 04:32:09 +0100

    Added UnifiableWith.

--- Nordril.TypeToolkit.Tests/TypeExtensionsTests.cs
+++ Nordril.TypeToolkit.Tests/TypeExtensionsTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
@@ -9,6 +10,142 @@ namespace Nordril.TypeToolkit.Tests
 {
     public static class TypeExtensionsTests
     {
+        public static IEnumerable<object[]> UnifiableWithData()
+        {
+            yield return new object[]
+            {
+                typeof(string),
+                typeof(string),
+                true,
+            };
+
+            yield return new object[]
+            {
+                typeof(int),
+                typeof(int),
+                true,
+            };
+
+            yield return new object[]
+            {
+                typeof(IEquatable<int>),
+                typeof(int),
+                true,
+            };
+
+            yield return new object[]
+            {
+                typeof(Manager),
+                typeof(Manager),
+                true,
+            };
+
+            yield return new object[]
+            {
+                typeof(Employee),
+                typeof(Manager),
+                true,
+            };
+
+            yield return new object[]
+            {
+                typeof(Person),
+                typeof(Manager),
+                true,
+            };
+
+            yield return new object[]
+            {
+                typeof(IEquatable<Person>),
+                typeof(Manager),
+                true,
+            };
+
+            yield return new object[]
+            {
+                typeof(MyList<>),
+                typeof(MyList<>),
+                true,
+            };
+
+            yield return new object[]
+            {
+                typeof(IEnumerable<>),
+                typeof(MyList<>),
+                true,
+            };
+
+            yield return new object[]
+            {
+                typeof(IEnumerable<int>),
+                typeof(MyList<int>),
+                true,
+            };
+
+            yield return new object[]
+            {
+                typeof(IComparable<>),
+                typeof(Manager),
+                false,
+            };
+
+            yield return new object[]
+            {
+                typeof(Manager),
+                typeof(Car),
+                false,
+            };
+
+            yield return new object[]
+            {
+                typeof(Manager),
+                typeof(IEnumerable<Manager>),
+                false,
+            };
+
+            yield return new object[]
+            {
+                typeof(IEquatable<Manager>),
+                typeof(Manager),
+                false,
+            };
+
+            yield return new object[]
+            {
+                typeof(Manager),
+                typeof(string),
+                false,
+            };
+
+            yield return new object[]
+            {
+                typeof(IEquatable<>),
+                typeof(int),
+                false,
+            };
+
+            yield return new object[]
+            {
+                typeof(Manager),
+                typeof(Employee),
+                false,
+            };
+
+            yield return new object[]
+            {
+                typeof(MyList<int>),
+                typeof(IEnumerable<int>),
+                false,
+            };
+
+            yield return new object[]
+            {
+                typeof(MyList<>),
+                typeof(IEnumerable<>),
+                false,
+            };
+        }
+
         [Fact]
         public static void GetBaseNameReturnsBaseName()
         {
@@ -77,9 +214,58 @@ namespace Nordril.TypeToolkit.Tests
             Assert.Equal("System.Collections.Generic.List<System.Collections.Generic.List<System.Collections.Generic.List<System.Int32>>>", typeof(List<List<List<int>>>).GetGenericName(true));
         }
 
+        [Theory]
+        [MemberData(nameof(UnifiableWithData))]
+        public static void UnifiableWithTest(Type type, Type that, bool expected)
+        {
+            var typeS = type.ToString();
+            var thatS = that.ToString();
+
+            if (expected)
+                Assert.True(type.UnifiableWith(that, out var _));
+            else
+                Assert.False(type.UnifiableWith(that, out var _));
+        }
+
         private class TestType
         {
             public T First<T>(IEnumerable<T> x) => x.First();
+        }
+
+        private class Manager : Employee
+        {
+            public int ManagerLevel { get; set; }
+        }
+
+        private class Employee : Person
+        {
+            public string Role { get; set; }
+        }
+
+        private class Person : IEquatable<Person>
+        {
+            public DateTime Birth { get; set; }
+            public string Name { get; set; }
+
+            public bool Equals(Person other) => Birth == other.Birth && Name == other.Name;
+        }
+
+        private class Car
+        {
+            public string Model { get; set; }
+        }
+
+        private class MyList<T> : IEnumerable<T>
+        {
+            public IEnumerator<T> GetEnumerator()
+            {
+                throw new NotImplementedException();
+            }
+
+            IEnumerator IEnumerable.GetEnumerator()
+            {
+                throw new NotImplementedException();
+            }
         }
     }
 }
--- Nordril.TypeToolkit/TypeExtensions.cs
+++ Nordril.TypeToolkit/TypeExtensions.cs
@@ -67,6 +67,89 @@ namespace Nordril.TypeToolkit
         public static Type GetGenericTypeDefinitionSafe(this Type type)
             => type.IsGenericType ? type.GetGenericTypeDefinition() : type;
 
+        public static IList<Type> GetSupertypes(this Type type)
+        {
+            var ret = new List<Type>();
+
+            foreach (var intf in type.GetInterfaces())
+            {
+                if (intf.IsGenericType && intf.GetGenericArguments().All(a => a.IsGenericParameter))
+                    ret.Add(intf.GetGenericTypeDefinition());
+                else
+                    ret.Add(intf);
+            }
+
+            while (!(type is null) && type != typeof(object))
+            {
+                type = type.BaseType;
+                if (type != null)
+                {
+                    if (type.IsGenericType && type.GetGenericArguments().All(a => a.IsGenericParameter))
+                        ret.Add(type.GetGenericTypeDefinition());
+                    else
+                        ret.Add(type);
+                }
+            }
+
+            return ret;
+        }
+
+        /// <summary>
+        /// A looser form of <see cref="Type.IsAssignableFrom(Type)"/> which also returns true if the open type <paramref name="type"/> implements the open interface or open base type <paramref name="that"/>.
+        /// </summary>
+        /// <param name="type">The first type which should be assignable to the second.</param>
+        /// <param name="that">The second type which should be assignable from the first.</param>
+        /// <param name="instantations">If <paramref name="type"/>is an open generic type and <paramref name="that"/> is closed, this list contains the types to which the type-parameters of <paramref name="type"/> have to be instantiated. Otherwise, this is null.</param>
+        /// <returns></returns>
+        public static bool UnifiableWith(this Type type, Type that, out IList<Type> instantations)
+        {
+            //rules:
+            //Un(t<>, t<>) -> true (equality)
+            //Un(t<X>, t<Y>) -> All[x in X] Un(x,y) (args-recursion)
+            //Un(t<>, c<> : ... b<> ...) -> Unifiable(t,b) (open/supertype)
+            //Un(t<X>, c<Y> : ... b<X> ...) -> Unifiable(t,b) (closed/supertype)
+            //otherwise: false
+
+            var typeGeneric = type.GetGenericTypeDefinitionSafe();
+            var typeArgs = type.GetGenericArgumentsSafe();
+            var thatGeneric = that.GetGenericTypeDefinitionSafe();
+            var thatArgs = that.GetGenericArgumentsSafe();
+
+            instantations = null;
+
+            //Case 1: base types match and are both open generic types
+            if (typeGeneric == thatGeneric && type.IsGenericTypeDefinition && that.IsGenericTypeDefinition)
+                return true;
+
+            //Case 2: the left type is a closed type and the right type is open
+            if (typeGeneric == thatGeneric && !type.IsGenericTypeDefinition && that.IsGenericTypeDefinition)
+            {
+                instantations = type.GetGenericArgumentsSafe();
+                return true;
+            }
+
+            //Case 2: base types match and are both constructed generic types
+            if (typeGeneric == thatGeneric && typeArgs.Length == thatArgs.Length && typeArgs.Zip(thatArgs, (x, y) => x.UnifiableWith(y, out var _)).All(x => x))
+                return true;
+
+            //Case 3: the right type matches a base type of the right type (open)
+            var thatSupertypes = GetSupertypes(thatGeneric);
+            var anyMatches = thatSupertypes.Any(t => UnifiableWith(typeGeneric, t, out var _));
+
+            if (anyMatches)
+                return true;
+
+            //Case 4: the right type matches a base type of the right type (open)
+            thatSupertypes = GetSupertypes(that);
+            anyMatches = thatSupertypes.Any(t => UnifiableWith(type, t, out var _));
+
+            if (anyMatches)
+                return true;
+
+            //Otherwise: false;
+            return false;
+        }
+
         /*public static TypeLike ToTreeRepresentation(this Type type)
         {
 
